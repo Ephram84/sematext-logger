5d903571cc541ae7aa19ed90df384f9883525107
Sebastian Herwig <sh@fino.digital>
2017-10-24 16:43:28 +0200

    Add InitLogger method

--- logger.go
+++ logger.go
@@ -3,8 +3,10 @@ package sematextlogger
 import (
 	"bytes"
 	"encoding/json"
+	"log"
 	"net/http"
 	"os"
+	"regexp"
 	"strings"
 )
 
@@ -16,7 +18,7 @@ type Logger struct {
 	URL      string
 }
 
-// NewLogger returns a logger
+// NewLogger returns a logger.
 func NewLogger(appToken string) *Logger {
 	host, _ := os.Hostname()
 	typ := "syslog"
@@ -24,52 +26,70 @@ func NewLogger(appToken string) *Logger {
 	return &Logger{AppToken: appToken, Type: typ, Host: host, URL: url}
 }
 
+// InitLogger inits a logger throught an enviroment variable that contains a url,
+// e.g., https://logsene-receiver.sematext.com/fzr64ktn-....
+func InitLogger(envVArName string) *Logger {
+	loggingURL := os.Getenv(envVArName)
+	appToken := ""
+	if loggingURL != "" {
+		log.Println("found", envVArName, ": ", loggingURL)
+		appToken = strings.Replace(loggingURL, "https://logsene-receiver.sematext.com/", "", -1)
+		appToken = strings.Replace(appToken, "/", "", -1)
+	} else {
+		panic("Url has not been found")
+	}
+
+	return NewLogger(appToken)
+}
+
+// WithType sets a new type.
 func (logger *Logger) WithType(t string) *Logger {
 	logger.Type = t
 	return logger
 }
 
+// WithType sets a new URL.
 func (logger *Logger) WithURL(url string) *Logger {
 	logger.URL = url
 	return logger
 }
 
 // Err logs a message with severity "err".
-func (logger *Logger) Err(msg string, additional ...string) (bool, error) {
+func (logger *Logger) Err(msg string, additional ...string) error {
 	return logger.buildMessage("err", msg, additional)
 }
 
 // Info logs a message with severity "info".
-func (logger *Logger) Info(msg string, additional ...string) (bool, error) {
+func (logger *Logger) Info(msg string, additional ...string) error {
 	return logger.buildMessage("info", msg, additional)
 }
 
 // Emerg logs a message with severity "emerg".
-func (logger *Logger) Emerg(msg string, additional ...string) (bool, error) {
+func (logger *Logger) Emerg(msg string, additional ...string) error {
 	return logger.buildMessage("emerg", msg, additional)
 }
 
 // Crit logs a message with severity "crit".
-func (logger *Logger) Crit(msg string, additional ...string) (bool, error) {
+func (logger *Logger) Crit(msg string, additional ...string) error {
 	return logger.buildMessage("crit", msg, additional)
 }
 
 // Warning logs a message with severity "warning".
-func (logger *Logger) Warning(msg string, additional ...string) (bool, error) {
+func (logger *Logger) Warning(msg string, additional ...string) error {
 	return logger.buildMessage("warning", msg, additional)
 }
 
 // Notice logs a message with severity "notice".
-func (logger *Logger) Notice(msg string, additional ...string) (bool, error) {
+func (logger *Logger) Notice(msg string, additional ...string) error {
 	return logger.buildMessage("notice", msg, additional)
 }
 
 // Debug logs a message with severity "debug".
-func (logger *Logger) Debug(msg string, additional ...string) (bool, error) {
+func (logger *Logger) Debug(msg string, additional ...string) error {
 	return logger.buildMessage("debug", msg, additional)
 }
 
-func (logger *Logger) buildMessage(severity, msg string, additional []string) (bool, error) {
+func (logger *Logger) buildMessage(severity, msg string, additional []string) error {
 	var message map[string]interface{}
 	message = make(map[string]interface{})
 
@@ -78,43 +98,48 @@ func (logger *Logger) buildMessage(severity, msg string, additional []string) (b
 	message["Host"] = logger.Host
 
 	for _, keyvalue := range additional {
-		parts := strings.Split(keyvalue, ":")
-		switch len(parts) {
-		case 1:
-			message["?"] = parts[0]
-		case 2:
-			message[parts[0]] = parts[1]
-		default:
-			value := parts[1]
-			for _, part := range parts[2:] {
-				value += ":" + part
+		r, _ := regexp.Compile("\\d{4}-\\d{2}-\\d{2}(T| )\\d{2}:\\d{2}:\\d{2}")
+		if r.MatchString(keyvalue) {
+			message["?"] = keyvalue
+		} else {
+			parts := strings.Split(keyvalue, ":")
+			switch len(parts) {
+			case 0, 1:
+				message["?"] = parts[0]
+			case 2:
+				message[parts[0]] = parts[1]
+			default:
+				value := parts[1]
+				for _, part := range parts[2:] {
+					value += ":" + part
+				}
+				message[parts[0]] = value
 			}
-			message[parts[0]] = value
+			// if len(parts) == 2 {
+			// 	message[parts[0]] = parts[1]
+			// }
 		}
-		// if len(parts) == 2 {
-		// 	message[parts[0]] = parts[1]
-		// }
 	}
 
 	jmsg, err := json.Marshal(message)
 	if err != nil {
-		return false, err
+		return err
 	}
 	return logger.sendMessage(jmsg)
 }
 
-func (logger *Logger) sendMessage(msg []byte) (bool, error) {
+func (logger *Logger) sendMessage(msg []byte) error {
 	req, err := http.NewRequest("POST", logger.URL+"/"+logger.AppToken+"/"+logger.Type, bytes.NewBuffer(msg))
 	if err != nil {
-		return false, err
+		return err
 	}
 	req.Header.Add("Content-Type", "application/json")
 
 	client := &http.Client{}
 	_, err = client.Do(req)
 	if err != nil {
-		return false, err
+		return err
 	}
 
-	return true, err
+	return err
 }
--- test/server_test.go
+++ test/server_test.go
@@ -2,8 +2,11 @@ package test
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 	"net/http/httptest"
+	"os"
+	"regexp"
 	"testing"
 	"time"
 
@@ -12,7 +15,7 @@ import (
 )
 
 const (
-	appToken string = "3cb2be30-05c6-45d6-bdc9-075cac545206"
+	appToken string = "430fv34u-05c6-45d6-bdc9-634dgez"
 	typ      string = "syslog"
 )
 
@@ -22,37 +25,73 @@ func TestServer(t *testing.T) {
 
 	logger := sematextlogger.NewLogger(appToken).WithType(typ).WithURL(ts.URL)
 
-	ok, err := logger.Err("An error has occurred", "path:/api/example/err")
-	check(t, ok, err)
+	err := logger.Err("An error has occurred", "path:/api/example/err")
+	check(t, err)
 
-	ok, err = logger.Info("test message", "Methode:GET", "uri:/api/test/info")
-	check(t, ok, err)
+	err = logger.Info("test message", "Methode:GET", "uri:/api/test/info")
+	check(t, err)
 
-	ok, err = logger.Debug("test message", "@timestamp:"+time.Now().String())
-	check(t, ok, err)
+	err = logger.Info("test message", "GET")
+	check(t, err)
 
-	ok, err = logger.Warning("test message", "Methode:GET", "uri:/api/test/info")
-	check(t, ok, err)
+	err = logger.Debug("test message", "@timestamp:"+time.Now().String())
+	check(t, err)
 
-	ok, err = logger.Notice("test message", "Methode:GET", "uri:/api/test/info")
-	check(t, ok, err)
+	err = logger.Debug("test message", time.Now().String())
+	check(t, err)
 
-	ok, err = logger.Crit("test message", "Methode:GET", "uri:/api/test/info")
-	check(t, ok, err)
+	err = logger.Warning("test message", "Methode:GET", "uri:/api/test/info")
+	check(t, err)
 
-	ok, err = logger.Emerg("test message", "Methode:GET", "uri:/api/test/info")
-	check(t, ok, err)
+	err = logger.Notice("test message", "Methode:GET", "uri:/api/test/info")
+	check(t, err)
+
+	err = logger.Crit("test message", "Methode:GET", "uri:/api/test/info")
+	check(t, err)
+
+	err = logger.Emerg("test message", "Methode:GET", "uri:/api/test/info")
+	check(t, err)
 }
 
-func check(t *testing.T, ok bool, err error) {
+func TestTime(t *testing.T) {
+	data := time.Now().UTC().String()
+
+	log.Println("now:", data)
+
+	r, err := regexp.Compile("\\d{4}-\\d{2}-\\d{2}(T| )\\d{2}:\\d{2}:\\d{2}")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	fmt.Println(r.MatchString(data))
+
+	dateTime := r.FindString(data)
+
+	fmt.Println("dateTime: ", dateTime)
+}
+
+func TestServerWithEnv(t *testing.T) {
+	//set enviroment variable for testing
+	os.Setenv("LOGGING_URL", "https://logsene-receiver.sematext.com/430fv34u-05c6-45d6-bdc9-634dgez/")
+
+	ts := httptest.NewServer(getHandler())
+	defer ts.Close()
+
+	logger := sematextlogger.InitLogger("LOGGING_URL").WithType(typ).WithURL(ts.URL)
+
+	err := logger.Err("An error has occurred", "path:/api/example/err")
+	check(t, err)
+}
+
+func check(t *testing.T, err error) {
 	if err != nil {
 		t.Error(err)
 	}
-	if !ok {
-		t.Error("Status code != 200")
-	}
 }
 
+//////////////////////////////////////
+//				Dummy				//
+//////////////////////////////////////
 func getHandler() *echo.Echo {
 	e := echo.New()
 
