f83f61eb2f93081c8c3825c6b201fdb01ecd01a0
Sebastian Herwig <sh@fino.digital>
2018-04-25 11:49:09 +0200

    Adjustment regarding test

--- test/server_test.go
+++ test/server_test.go
@@ -1,18 +1,11 @@
 package test
 
 import (
-	"io"
-	"log"
 	"net/http/httptest"
 	"os"
 	"testing"
 
 	sematextlogger "github.com/Ephram84/sematext-logger"
-	"github.com/Ephram84/sematext-logger/logwriter"
-)
-
-const (
-	appToken string = "3cb2be30-05c6-45d6-bdc9-075cac545206"
 )
 
 func TestServer(t *testing.T) {
@@ -28,22 +21,8 @@ func TestServer(t *testing.T) {
 }
 
 func TestDialSematext(t *testing.T) {
-	logger := sematextlogger.NewLogger(appToken, "test")
+	logger := sematextlogger.NewLogger(os.Getenv("appToken"), "test")
 
 	// logger.Err("An error has occurred")
-	logger.Info("Info")
-}
-
-func TestLogger(t *testing.T) {
-	sematext, _ := logwriter.Dial("udp", "logsene-receiver-syslog.sematext.com:514", logwriter.LOG_LOCAL0, appToken)
-
-	multi := io.MultiWriter(sematext, os.Stdout)
-
-	info := log.New(multi, "INFO: ", log.Ldate|log.Ltime|log.Lshortfile)
-
-	info.Println("An error has occurred")
-}
-
-func TestEnv(t *testing.T) {
-	println(os.Getenv("LOGGING_URL"))
+	logger.Info("Info", "It's just a info message")
 }
